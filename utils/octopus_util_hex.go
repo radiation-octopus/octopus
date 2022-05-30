2b25a4c7cba9af1228ae2724c7782a3e6d50887a
MCcao <mc_cao@foxmail.com>
2022-05-30 23:36:50 +0800

    utils update

--- utils/octopus_util_hex.go
+++ utils/octopus_util_hex.go
@@ -9,3 +9,21 @@ func Encode(b []byte) string {
 	hex.Encode(enc[2:], b)
 	return string(enc)
 }
+
+// BytesToHash sets b to hash.
+// If b is larger than len(h), b will be cropped from the left.
+
+func Hex2Bytes(str string) []byte {
+	h, _ := hex.DecodeString(str)
+	return h
+}
+
+// CopyBytes返回所提供字节的精确副本。
+func CopyBytes(b []byte) (copiedBytes []byte) {
+	if b == nil {
+		return nil
+	}
+	copiedBytes = make([]byte, len(b))
+	copy(copiedBytes, b)
+	return
+}
